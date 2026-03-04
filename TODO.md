c7b9247e7a19a2340fbaf5d1b37fb4aa4e5d9aaf
Samuel Ytterbrink <neppord@gmail.com>
2026-03-04 15:30:35 +0100

    Take direction out of _physics process
    Co-authored-by: Karl Wängberg <karlwangberg@gmail.com>

--- TODO.md
+++ TODO.md
@@ -45,6 +45,7 @@ Co-authored-by: Karl Wängberg <karlwangberg@gmail.com>
 * [x] Use signals
   * [x] player 1 toy
   * [x] player 2 toy
+  * [ ] Improvements
 
 * [ ] Have sendable toys
   * [ ] Send a toy with currently charged battery with space/enter
--- toy.gd
+++ toy.gd
@@ -2,12 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
+var direction := Vector2.RIGHT
 
 func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Vector2.RIGHT
 	velocity = direction * SPEED
 	move_and_slide()
