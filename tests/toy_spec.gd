5a23d6721914fc9cc374301175414a990ab775eb
Samuel Ytterbrink <neppord@gmail.com>
2026-04-19 15:43:21 +0200

    add type hints to toy_spec.gd

--- tests/toy_spec.gd
+++ tests/toy_spec.gd
@@ -2,7 +2,7 @@
 var spec: Spec = Spec.new("Toy", func(t: Runner):
     t.describe("defaults", func(t: Runner):
         t.it("has a charge of 1", func(t: Runner):
-            var toy = Toy.new()
+            var toy: Toy = Toy.new()
             assert(toy.charge == 1, "charge should be one")
             toy.free()
         )
@@ -10,8 +10,8 @@ var spec: Spec = Spec.new("Toy", func(t: Runner):
 
     t.describe("physics process", func(t: Runner):
         t.it("depletes charge over time", func(t: Runner):
-            var toy = Toy.new()
-            var initial_charge = toy.charge
+            var toy: Toy = Toy.new()
+            var initial_charge: float = toy.charge
             toy._physics_process(1.0)
             assert(toy.charge < initial_charge, "charge should decrease")
             assert(abs(toy.charge - (initial_charge - 1.0/15)) < 0.001, "charge should decrease by delta/15")
@@ -19,7 +19,7 @@ var spec: Spec = Spec.new("Toy", func(t: Runner):
         )
 
         t.it("sets linear velocity based on charge and speed", func(t: Runner):
-            var toy = Toy.new()
+            var toy: Toy = Toy.new()
             toy.charge = 0.5
             toy.direction = Vector2.RIGHT
             # delta=0 to check velocity without depleting charge
@@ -31,8 +31,8 @@ var spec: Spec = Spec.new("Toy", func(t: Runner):
 
     t.describe("visuals", func(t: Runner):
         t.it("flips sprite based on direction", func(t: Runner):
-            var toy = Toy.new()
-            var sprite = Sprite2D.new()
+            var toy: Toy = Toy.new()
+            var sprite: Sprite2D = Sprite2D.new()
             toy.sprite = sprite
             
             toy.direction = Vector2.RIGHT
